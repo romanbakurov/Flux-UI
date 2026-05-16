8fd331a9d54501f0d7e375a240fc8f347f465b50
romanbakurov <ibakur@ya.ru>
2026-05-16 18:01:52 +0200

    fix: update ESLint config and fix linting errors

--- eslint.config.js
+++ eslint.config.js
@@ -10,11 +10,28 @@ export default [
   ...storybook.configs['flat/recommended'],
   {
     files: ['src/**/*.{ts,tsx}'],
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooks,
+    },
     rules: {
+      // Кавычки
       quotes: ['error', 'single', { avoidEscape: true }],
+
+      // TypeScript
       '@typescript-eslint/no-unused-vars': ['error'],
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+
+      // React
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
+
+      // Storybook
+      'storybook/story-exports': 'off',
     },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**', '*.config.js', '*.config.ts'],
   },
 ];
