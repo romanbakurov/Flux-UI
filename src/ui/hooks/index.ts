56f3c97461f59f1946d2a0201085dfd4cae20b90
romanbakurov <ibakur@ya.ru>
2026-06-04 23:44:57 +0200

    fix: deleted usePortal

--- src/ui/hooks/index.ts
+++ src/ui/hooks/index.ts
@@ -3,4 +3,3 @@ export { useOutsideClick } from './useOutsideClick';
 export { useFloatingPosition } from './useFloatingPosition';
 export { useKeyboardNavigation } from './useKeyboardNavigation';
 export { useFocusManager } from './useFocusManager';
-export { usePortal } from './usePortal';
