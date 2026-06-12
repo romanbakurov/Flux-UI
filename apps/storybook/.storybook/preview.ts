70e5743f5399978a0a35b22e4b693d965467329d
romanbakurov <ibakur@ya.ru>
2026-06-12 22:40:53 +0200

    ci(chromatic): optimize install steps and build order

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -1,6 +1,6 @@
 import type { Preview } from '@storybook/react-vite';
 
-import '../../../packages/flux-ui-web/src/styles/global.scss';
+import '@romanbakurov/flux-ui-web/styles';
 
 const preview: Preview = {};
 
