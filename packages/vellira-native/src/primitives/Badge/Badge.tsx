c44422dd078bf952ccfea4cc1b15939314c2c652
Roman Bakurov <ronbakurov@gmail.com>
2026-06-18 21:44:26 +0200

    docs(storybook): improve input documentation (#70)

--- packages/vellira-native/src/primitives/Badge/Badge.tsx
+++
@@ -1,7 +0,0 @@
-import { Text } from 'react-native';
-
-import type { BadgeProps } from './types';
-
-export function Badge(_props: BadgeProps) {
-  return <Text>Badge</Text>;
-}
--- packages/vellira-web/src/primitives/index.ts
+++ packages/vellira-web/src/primitives/index.ts
@@ -1,4 +1,3 @@
-export * from './Badge';
 export * from './Button';
 export * from './Checkbox';
 export * from './Input';
